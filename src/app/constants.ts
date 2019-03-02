8b8dd8eee0f6d0d169bdc5b377f1d3439455f7f9
debjohnson33 <debjohnson_11@yahoo.com>
2019-03-02 15:41:24 -0600

    Edited constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,5 @@
 export const constants = {
   PROFILE_PIC:
+// tslint:disable-next-line:max-line-length
 'https://firebasestorage.googleapis.com/v0/b/myapp-4eadd.appspot.com/o/chatterplace.png?alt=media&token=e51fa887-bfc6-48ff-87c6-e2c61976534e'
 };
