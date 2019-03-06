789b43044a17376e4e494a4583e4787c8affea62
debjohnson33 <debjohnson_11@yahoo.com>
2019-03-06 16:19:11 -0600

    Added login function to login page component

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -29,6 +29,10 @@ export class LoginPageComponent implements OnInit {
   ngOnInit() {
   }
 
+  login() {
+    this.auth.login(this.usercreds);
+  }
+
   signup() {
     this.router.navigate(['signup']);
   }
