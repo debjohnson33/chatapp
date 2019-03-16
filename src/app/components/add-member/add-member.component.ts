4a602a4b971c11b39b2872303e8b41b165d51b8e
debjohnson33 <debjohnson_11@yahoo.com>
2019-03-16 03:35:41 -0500

    Added friend retrieval/edited add-member html/css

--- src/app/components/add-member/add-member.component.ts
+++ src/app/components/add-member/add-member.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { GroupsService } from '../../services/groups.service';
+import { UserService } from '../../services/user.service';
+import { FriendsService } from '../../services/friends.service';
 
 @Component({
   selector: 'app-add-member',
   templateUrl: './add-member.component.html',
   styleUrls: ['./add-member.component.css']
 })
+
 export class AddMemberComponent implements OnInit {
 
-  constructor() { }
+  loadingSpinner = false;
+  myFriends = [];
+
+  constructor(private friendsService: FriendsService,
+              private groupsService: GroupsService,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.friendsService.getMyFriends().then((value: any) => {
+      this.loadingSpinner = true;
+      value.subscribe((friendsemail) => {
+        if (friendsemail === 'Exists') {
+          this.friendsService.getFriendList().subscribe((friends) => {
+            this.userService.getUserDetails(friends).then((friendDetails: any) => {
+              this.myFriends = friendDetails;
+            });
+          });
+        }
+      });
+    });
   }
 
 }
