435468f76c5bfd6754a1a723cbacfdf063384bd9
debjohnson33 <debjohnson_11@yahoo.com>
2019-03-19 16:06:44 -0500

    Edited group footer html/css/component

--- src/app/components/group-footer/group-footer.component.ts
+++ src/app/components/group-footer/group-footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessagesService } from '../../services/messages.service';
 
 @Component({
   selector: 'app-group-footer',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupFooterComponent implements OnInit {
 
-  constructor() { }
+  newMessage: string;
+  picMessage: FileList;
+
+  constructor(private msgService: MessagesService) { }
 
   ngOnInit() {
+  }
+
+  addMessage() {
+    if (this.newMessage !== '') {
+      this.msgService.addNewMsg(this.newMessage);
+      this.newMessage = '';
+    }
+  }
+
+  sendImage(event) {
+    this.picMessage = event.target.files;
+    if (this.picMessage.item(0)) {
+      this.msgService.addPicMsg(this.picMessage.item(0));
+    }
   }
 
 }
