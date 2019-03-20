a90ceb5e43c491eb28f83adc5ea2762bfaed76cf
debjohnson33 <debjohnson_11@yahoo.com>
2019-03-19 23:30:47 -0500

    Edited notifications html/onInit

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FriendsService } from '../../services/friends.service';
+import { UserService } from '../../services/user.service';
+import { MessagesService } from '../../services/messages.service';
 
 @Component({
   selector: 'app-notifications',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsComponent implements OnInit {
 
-  constructor() { }
+  myNotifications = [];
+  currentChatUser;
+  showNotifications = false;
+
+  constructor(private messagesService: MessagesService,
+              private friendsService: FriendsService) { }
 
   ngOnInit() {
+    this.messagesService.enteredChat.subscribe((value) => {
+      if (value) {
+        this.currentChatUser = this.messagesService.currentChatUser;
+        this.messagesService.clearNotifications();
+      }
+    });
+    this.messagesService.getMyNotifications().subscribe((notifications) => {
+      this.showNotifications = false;
+      if (this.currentChatUser !== undefined) {
+        notifications.forEach((element: any, i) => {
+          if (element.sender === this.currentChatUser.email) {
+            notifications.splice(i, 1);
+            this.messagesService.clearNotifications();
+          }
+        });
+      }
+      this.myNotifications = notifications;
+      this.showNotifications = true;
+    });
   }
 
 }
